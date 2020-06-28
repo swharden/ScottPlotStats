e2006281e65f02443a9100fe0cc881fcc507595b
Scott W Harden <swharden@gmail.com>
2020-06-28 11:10:56 -0400

    create plots with real data

--- src/Functions.cs
+++ src/Functions.cs
@@ -1,10 +1,12 @@
 using System;
-using System.Diagnostics;
 using System.Linq;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using System.Drawing;
 using Microsoft.Azure.Cosmos.Table;
+using Azure.Storage.Blobs;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace PackagePopularityTracker
 {
@@ -16,12 +18,14 @@ namespace PackagePopularityTracker
             string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage", EnvironmentVariableTarget.Process);
             CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
             CloudTableClient tableClient = cloudStorageAccount.CreateCloudTableClient();
-
-            CloudTable tableNames = tableClient.GetTableReference("packageNames");
-            string[] packageNames = GetPackageNames(tableNames, log);
-
-            CloudTable tableStats = tableClient.GetTableReference("packageStats");
             DateTime timeStamp = DateTime.UtcNow;
+
+            // read package names
+            CloudTable namesTable = tableClient.GetTableReference("packageNames");
+            string[] packageNames = namesTable.CreateQuery<PackageName>().Select(x => x.RowKey).ToArray();
+
+            // record stats for each package
+            CloudTable statsTable = tableClient.GetTableReference("packageStats");
             foreach (string packageName in packageNames)
             {
                 PackageStat packageStat = new PackageStat()
@@ -32,7 +36,7 @@ namespace PackagePopularityTracker
                 };
 
                 TableOperation insertOrMergeOperation = TableOperation.InsertOrMerge(packageStat);
-                TableResult result = tableStats.Execute(insertOrMergeOperation);
+                TableResult result = statsTable.Execute(insertOrMergeOperation);
                 PackageStat insertedPackageStat = result.Result as PackageStat;
                 log.LogInformation($"Inserted: {insertedPackageStat}");
             }
@@ -41,21 +45,45 @@ namespace PackagePopularityTracker
         [FunctionName("UpdatePackagePlots")] // run on minute 2 of every hour
         public static void UpdatePackagePlots([TimerTrigger("0 2 * * * *")] TimerInfo myTimer, ILogger log)
         {
-            /*
             string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage", EnvironmentVariableTarget.Process);
             CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
-            CloudTableClient tableClient = cloudStorageAccount.CreateCloudTableClient();
-            CloudTable table = tableClient.GetTableReference("packageStats");
-            string[] packageNames = GetPackageNames(table, log);
-            */
-        }
 
-        private static string[] GetPackageNames(CloudTable table, ILogger log)
-        {
-            string[] packageNames = table.CreateQuery<PackageName>().Select(x => x.RowKey).ToArray();
-            Array.Sort(packageNames);
-            log.LogInformation($"Identified {packageNames.Length} packages: " + string.Join(", ", packageNames));
-            return packageNames;
+            // get all stats
+            CloudTableClient tableClient = cloudStorageAccount.CreateCloudTableClient();
+            CloudTable statsTable = tableClient.GetTableReference("packageStats");
+            PackageStat[] allStats = statsTable.CreateQuery<PackageStat>().ToArray().OrderBy(x => x.Timestamp).ToArray();
+            string[] packageNames = allStats.Select(x => x.Package).Distinct().ToArray();
+            log.LogInformation($"Found {allStats.Length} total records.");
+            log.LogInformation($"Identified {packageNames.Length} unique packages.");
+
+            // create individual plots
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("$web");
+            foreach (string packageName in packageNames)
+            {
+                PackageStat[] stats = allStats.Where(x => x.Package == packageName).ToArray();
+                log.LogInformation($"Package {packageName} has {stats.Length} records.");
+                double[] datetimes = stats.Select(x => x.Timestamp.DateTime.ToOADate()).ToArray();
+                double[] downloads = stats.Select(x => (double)x.Downloads).ToArray();
+
+                var plt = new ScottPlot.Plot(600, 400);
+                plt.Title(packageName);
+                plt.YLabel("Downloads");
+                plt.PlotScatter(datetimes, downloads);
+                plt.Ticks(dateTimeX: true);
+
+                string filePath = $"packagestats/{packageName}.png";
+                BlobClient blobClient = containerClient.GetBlobClient(filePath);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    Bitmap bmp = plt.GetBitmap(true);
+                    bmp.Save(memoryStream, ImageFormat.Png);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                    blobClient.Upload(memoryStream, overwrite: true);
+                }
+
+                log.LogInformation($"Created {filePath}");
+            }
         }
     }
 }
