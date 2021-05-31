7e54ec07629679f77ddb2ade6e31e66b787a17c4
Scott W Harden <swharden@gmail.com>
2021-05-31 12:09:20 -0400

    JSON IO for log format

---
+++ src/NuGetPPT.Tests/IoTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuGetPPT.Tests
+{
+    class IoTests
+    {
+        [Test]
+        public void Test_Logs_ReadCorrectly()
+        {
+            string json = SampleData.ReadSampleData("logs-scottplot.json");
+            var downloads = IO.FromJson(json);
+            Assert.AreEqual(343, downloads.Count);
+
+            foreach (string ts in downloads.Keys)
+                Console.WriteLine($"{ts}\t{downloads[ts]}");
+        }
+
+        [Test]
+        public void Test_Logs_AreConsecutive()
+        {
+            string json = SampleData.ReadSampleData("logs-scottplot.json");
+            var downloads = IO.FromJson(json);
+
+            var last = new DateTime();
+            foreach (string ts in downloads.Keys)
+            {
+                DateTime dt = DateTime.Parse(ts);
+                Assert.Greater(dt, last);
+                last = dt;
+            }
+        }
+
+        [Test]
+        public void Test_Logs_EncodeIdentically()
+        {
+            string jsonInput = SampleData.ReadSampleData("logs-scottplot.json");
+
+            Dictionary<string, int> records = IO.FromJson(jsonInput);
+            string jsonOutput = IO.ToJson(records, "scottplot");
+
+            Assert.AreEqual(jsonInput, jsonOutput);
+        }
+    }
+}
---
+++ src/NuGetPPT/IO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace NuGetPPT
+{
+    public static class IO
+    {
+        /// <summary>
+        /// Create a dictionary of timestamp:downloads from JSON
+        /// </summary>
+        public static Dictionary<string, int> FromJson(string json)
+        {
+            using JsonDocument document = JsonDocument.Parse(json);
+            return document.RootElement.GetProperty("records")
+                .EnumerateObject()
+                .ToDictionary(x => x.Name, x => int.Parse(x.Value.ToString()));
+        }
+
+        /// <summary>
+        /// Create JSON from a dictionary of timestamp:downloads
+        /// </summary>
+        public static string ToJson(Dictionary<string, int> downloads, string packageName)
+        {
+            using var stream = new MemoryStream();
+            using var writer = new Utf8JsonWriter(stream);
+
+            writer.WriteStartObject();
+            writer.WriteString("package", packageName);
+            writer.WriteStartObject("records");
+            foreach (string timestamp in downloads.Keys)
+                writer.WriteNumber(timestamp, downloads[timestamp]);
+            writer.WriteEndObject();
+            writer.WriteEndObject();
+
+            writer.Flush();
+            return Encoding.UTF8.GetString(stream.ToArray());
+        }
+    }
+}
