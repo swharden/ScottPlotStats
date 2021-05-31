af948af7304a823b81bfc3f23ffc97997e6d9715
Scott W Harden <swharden@gmail.com>
2021-05-31 13:22:35 -0400

    add timestamp to plots

--- src/NuGetPPT/Plotting.cs
+++ src/NuGetPPT/Plotting.cs
@@ -19,6 +19,16 @@ namespace NuGetPPT
             plt.XAxis.DateTimeFormat(true);
             plt.YLabel("Total Downloads");
 
+            string day = DateTime.UtcNow.ToString("yyyy-MM-dd");
+            string time = DateTime.UtcNow.ToString("H:mm");
+            var dateAnnotation = plt.AddAnnotation($"updated {day} at {time} UTC", -3, -1);
+            dateAnnotation.Border = false;
+            dateAnnotation.Shadow = false;
+            dateAnnotation.Background = false;
+            dateAnnotation.Font.Color = System.Drawing.Color.FromArgb(150, System.Drawing.Color.Black);
+            dateAnnotation.Font.Name = ScottPlot.Drawing.InstalledFont.Monospace();
+            dateAnnotation.Font.Size = 10;
+
             return plt.Render();
         }
 
