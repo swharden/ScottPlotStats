594b04d10bf38978b496373e46b57e6429c63596
Scott W Harden <swharden@gmail.com>
2024-04-17 17:10:32 -0400

    Tests: update test folders

--- src/ScottPlotStats.Test/NuGetPlottingTests.cs
+++ src/ScottPlotStats.Test/NuGetPlottingTests.cs
@@ -6,8 +6,11 @@ public class NuGetPlottingTests
     [TestMethod]
     public void Test_Plot_CountsOverTime()
     {
+        string saveFolder = Path.GetFullPath("./test-images");
+        Directory.CreateDirectory(saveFolder);
+
         CountDatabase db = SampleData.CountDatabase;
-        NuGetPlotting.DownloadCount(db).SavePng("DownloadCount.png", 600, 350);
-        NuGetPlotting.DownloadRate(db).SavePng("DownloadRate.png", 600, 350);
+        NuGetPlotting.DownloadCount(db).SavePng(Path.Join(saveFolder, "DownloadCount.png"), 600, 350);
+        NuGetPlotting.DownloadRate(db).SavePng(Path.Join(saveFolder, "DownloadRate.png"), 600, 350);
     }
 }
--- src/ScottPlotStats.Test/StarPlottingTests.cs
+++ src/ScottPlotStats.Test/StarPlottingTests.cs
@@ -6,10 +6,13 @@ public class StarPlottingTests
     [TestMethod]
     public void Test_Star_Plotting()
     {
+        string saveFolder = Path.GetFullPath("./test-images");
+        Directory.CreateDirectory(saveFolder);
+
         GitHubStarPlotting plotter = new(SampleData.StarCollection);
-        plotter.StarsOverTime().SavePng("star-count.png", 600, 400);
-        plotter.NewStarsPerWeek().SavePng("star-new-week.png", 600, 400);
-        plotter.NewStarsPerMonth().SavePng("star-new-month.png", 600, 400);
-        Console.WriteLine($"Saved images in: {Path.GetFullPath("./")}");
+        plotter.StarsOverTime().SavePng(Path.Join(saveFolder, "star-count.png"), 600, 400);
+        plotter.NewStarsPerWeek().SavePng(Path.Join(saveFolder, "star-new-week.png"), 600, 400);
+        plotter.NewStarsPerMonth().SavePng(Path.Join(saveFolder, "star-new-month.png"), 600, 400);
+        Console.WriteLine($"Saved images in: {saveFolder}");
     }
 }
