5b786d313206031f7c67376809e025815bfe0383
Scott W Harden <swharden@gmail.com>
2024-11-11 18:21:31 -0500

    Tests: allow star API to save file

--- src/ScottPlotStats.Test/StarApiTests.cs
+++ src/ScottPlotStats.Test/StarApiTests.cs
@@ -6,9 +6,16 @@ public class StarApiTests
     [TestMethod]
     public void Test_Star_Api()
     {
+        bool updateDevFile = false;
         GitHubDataFetcher api = new("scottplot", "scottplot");
-        GitHubStarsCollection stars1 = api.GetStars(3);
+        GitHubStarsCollection stars1 = api.GetStars(updateDevFile ? 9999 : 3);
         stars1.Count.Should().BeGreaterThan(123);
+        if (updateDevFile)
+        {
+            string saveAs = Path.GetFullPath(SampleData.StarsJsonFilePath);
+            File.WriteAllText(saveAs, stars1.ToJson());
+            Console.WriteLine($"Saved: {saveAs}");
+        }
 
         string json = stars1.ToJson();
         GitHubStarsCollection stars2 = GitHubStarsCollection.FromJson(json);
